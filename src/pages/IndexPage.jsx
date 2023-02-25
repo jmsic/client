b5c932bb554a1e3fc315bc675caa3d74ace0eea6
Ian Chen <jmsic@hotmail.com>
2023-02-25 12:24:02 -0800

    fix the router

---
+++ src/pages/IndexPage.jsx
@@ -0,0 +1,37 @@
+export default function IndexPage(){
+    return (
+        <div>
+        <header className='flex justify-between'>
+            <a href="" className='flex items-center gap-1'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9" />
+                </svg>
+                <span className='font-bold text-xl'>AirCnC</span>
+            </a>
+            <div className='flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300'>
+                <div>Anywhere</div>
+                <div className='border border-1 border-gray-300'></div>
+                <div>Any Week</div>
+                <div className='border border-1 border-gray-300'></div>
+                <div>Add Guests</div>
+                <button className='bg-primary text-white p-1 rounded-full'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                    </svg>
+                </button>
+            </div>
+            <a href="" className='flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-gray-300'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+                <div className='bg-gray-500 text-white rounded-full border border-gray-500 overflow-hidden'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 relative top-1">
+                        <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
+                    </svg>
+                </div>
+            </a>
+        </header>
+    </div>
+    )
+}
+
